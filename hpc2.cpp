8fc8005a4fe6ea634479794f587598a4302695d3
Anagha Rajput <111584300+Anagha8302@users.noreply.github.com>
2024-04-23 23:09:55 +0530

    hpc2.cpp

--- hpc2.cpp
+++ hpc2.cpp
@@ -73,62 +73,121 @@ int main()
 /*----------------------------------------------------------------------------------mergeSOrt----------------------------*/
 
 #include <iostream>
-#include <ctime>
 #include <cstdlib>
+#include <ctime>
 #include <omp.h>
-
 using namespace std;
 
 void merge(int arr[], int l, int m, int r) {
-    // Merge function for Merge Sort
+    int i, j, k;
+    int n1 = m - l + 1;
+    int n2 = r - m;
+
+    int *L = new int[n1];
+    int *R = new int[n2];
+
+    for (i = 0; i < n1; ++i) {
+        L[i] = arr[l + i];
+    }
+
+    for (j = 0; j < n2; ++j) {
+        R[j] = arr[m + 1 + j];
+    }
+
+    i = 0;
+    j = 0;
+    k = l;
+
+    while (i < n1 && j < n2) {
+        if (L[i] <= R[j]) {
+            arr[k] = L[i];
+            ++i;
+        } else {
+            arr[k] = R[j];
+            ++j;
+        }
+        ++k;
+    }
+
+    while (i < n1) {
+        arr[k] = L[i];
+        ++i;
+        ++k;
+    }
+
+    while (j < n2) {
+        arr[k] = R[j];
+        ++j;
+        ++k;
+    }
+
+    delete[] L;
+    delete[] R;
 }
 
-void mergeSort(int arr[], int l, int r) {
-    // Merge sort algorithm implementation
+void mergeSortSequential(int arr[], int l, int r) {
     if (l < r) {
         int m = l + (r - l) / 2;
-        mergeSort(arr, l, m);
-        mergeSort(arr, m + 1, r);
+        mergeSortSequential(arr, l, m);
+        mergeSortSequential(arr, m + 1, r);
         merge(arr, l, m, r);
     }
 }
 
-double measureTime(void (*sortFunction)(int[], int, int), int arr[], int n) {
-    // Function to measure execution time of sorting algorithms
-    clock_t start = clock();
-    sortFunction(arr, 0, n - 1);
-    clock_t end = clock();
-    return double(end - start) / CLOCKS_PER_SEC;
+void mergeSortParallel(int arr[], int l, int r) {
+    if (l < r) {
+        int m = l + (r - l) / 2;
+#pragma omp parallel sections
+        {
+#pragma omp section
+            {
+                mergeSortParallel(arr, l, m);
+            }
+#pragma omp section
+            {
+                mergeSortParallel(arr, m + 1, r);
+            }
+        }
+        merge(arr, l, m, r);
+    }
+}
+
+void printArray(int arr[], int size) {
+    for (int i = 0; i < size; i++) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
 }
 
 int main() {
     int n;
-    cout << "Enter the size of the array: ";
+    cout << "Enter size of array: ";
     cin >> n;
 
-    int *arr = new int[n];
-    int *arr_copy = new int[n];
+    int *arr_seq = new int[n];
+    int *arr_par = new int[n];
 
     srand(time(0));
-
-    for (int i = 0; i < n; ++i) {
+    for (int i = 0; i < n; i++) {
         int rand_num = rand() % 100;
-        arr[i] = rand_num;
-        arr_copy[i] = rand_num;
+        arr_seq[i] = rand_num;
+        arr_par[i] = rand_num;
     }
 
-    double sequentialMergeTime, parallelMergeTime;
+    clock_t start_seq = clock();
+    mergeSortSequential(arr_seq, 0, n - 1);
+    clock_t end_seq = clock();
+    double sequentialMS = double(end_seq - start_seq) / CLOCKS_PER_SEC;
+    cout << "Sequential time: " << sequentialMS << " seconds" << endl;
 
-    // Sequential Merge Sort
-    sequentialMergeTime = measureTime(mergeSort, arr, n);
-    cout << "Sequential Merge Sort Time: " << sequentialMergeTime << " seconds" << endl;
-
-    // Parallel Merge Sort
-    parallelMergeTime = measureTime(mergeSort, arr_copy, n);
+    clock_t start_par = clock();
+    mergeSortParallel(arr_par, 0, n - 1);
+    clock_t end_par = clock();
+    double parallelMergeTime = double(end_par - start_par) / CLOCKS_PER_SEC;
     cout << "Parallel Merge Sort Time: " << parallelMergeTime << " seconds" << endl;
 
-    delete[] arr;
-    delete[] arr_copy;
+    delete[] arr_seq;
+    delete[] arr_par;
 
     return 0;
 }
