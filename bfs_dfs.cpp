58afdb89ebc690d1c9d2cb516340cac41523f3d9
Anagha Rajput <111584300+Anagha8302@users.noreply.github.com>
2024-04-23 11:26:12 +0530

    bfs_dfs.cpp

--- bfs_dfs.cpp
+++ bfs_dfs.cpp
@@ -5,7 +5,6 @@
 
 using namespace std;
 
-// Graph class representing the adjacency list
 class Graph {
     int V;  // Number of vertices
     vector<vector<int>> adj;  // Adjacency list
@@ -13,31 +12,10 @@ class Graph {
 public:
     Graph(int V) : V(V), adj(V) {}
 
-    // Add an edge to the graph
     void addEdge(int v, int w) {
         adj[v].push_back(w);
     }
 
-    // Parallel Depth-First Search
-    void parallelDFS(int startVertex) {
-        vector<bool> visited(V, false);
-        parallelDFSUtil(startVertex, visited);
-    }
-
-    // Parallel DFS utility function
-    void parallelDFSUtil(int v, vector<bool>& visited) {
-        visited[v] = true;
-        cout << v << " ";
-
-        #pragma omp parallel for
-        for (int i = 0; i < adj[v].size(); ++i) {
-            int n = adj[v][i];
-            if (!visited[n])
-                parallelDFSUtil(n, visited);
-        }
-    }
-
-    // Parallel Breadth-First Search
     void parallelBFS(int startVertex) {
         vector<bool> visited(V, false);
         queue<int> q;
@@ -50,12 +28,15 @@ public:
             q.pop();
             cout << v << " ";
 
-            #pragma omp parallel for
+#pragma omp parallel for
             for (int i = 0; i < adj[v].size(); ++i) {
                 int n = adj[v][i];
                 if (!visited[n]) {
-                    visited[n] = true;
-                    q.push(n);
+#pragma omp critical
+                    {
+                        visited[n] = true;
+                        q.push(n);
+                    }
                 }
             }
         }
@@ -63,7 +44,6 @@ public:
 };
 
 int main() {
-    // Create a graph
     Graph g(7);
     g.addEdge(0, 1);
     g.addEdge(0, 2);
@@ -71,7 +51,6 @@ int main() {
     g.addEdge(1, 4);
     g.addEdge(2, 5);
     g.addEdge(2, 6);
-    
     /*
         0 -------->1
         |         / \
@@ -84,13 +63,70 @@ int main() {
         v      v
         5      6
     */
+    cout << "Breadth-First Search (BFS): ";
+    g.parallelBFS(0);
+    cout << endl;
+
+    return 0;
+}
+/*--------------------------dfs-----------------------*/
+#include <iostream>
+#include <vector>
+#include <stack>
+#include <omp.h>
+
+using namespace std;
+
+class Graph {
+    int V;  // Number of vertices
+    vector<vector<int>> adj;  // Adjacency list
+
+public:
+    Graph(int V) : V(V), adj(V) {}
+
+    void addEdge(int v, int w) {
+        adj[v].push_back(w);
+    }
+
+    void parallelDFS(int startVertex) {
+        vector<bool> visited(V, false);
+        stack<int> stk;
+
+        stk.push(startVertex);
+        while (!stk.empty()) {
+            int v = stk.top();
+            stk.pop();
+
+            if (!visited[v]) {
+                cout << v << " ";
+                visited[v] = true;
+
+#pragma omp parallel for
+                for (int i = 0; i < adj[v].size(); ++i) {
+                    int n = adj[v][i];
+                    if (!visited[n]) {
+#pragma omp critical
+                        {
+                            stk.push(n);
+                        }
+                    }
+                }
+            }
+        }
+    }
+};
+
+int main() {
+    Graph g(7);
+    g.addEdge(0, 1);
+    g.addEdge(0, 2);
+    g.addEdge(1, 3);
+    g.addEdge(1, 4);
+    g.addEdge(2, 5);
+    g.addEdge(2, 6);
 
     cout << "Depth-First Search (DFS): ";
     g.parallelDFS(0);
-    cout << endl;
-
-    cout << "Breadth-First Search (BFS): ";
-    g.parallelBFS(0);
     cout << endl;
 
     return 0;
